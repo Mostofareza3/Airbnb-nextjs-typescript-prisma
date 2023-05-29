68fd048b4c057a77a2191c6f96e6d38a6b29ce97
unknown <mostofa.reza3@gmail.com>
2023-05-30 00:41:12 +0600

    [Modal & Button done]

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Nunito } from "next/font/google";
@@ -19,8 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
-        {children}
+        <ClientOnly>
+          <Navbar />
+          <Modal title="Filters" isOpen />
+          {/* {children} */}
+        </ClientOnly>
       </body>
     </html>
   );
