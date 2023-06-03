19a178192699a199c84968bfeecb34f57fb31067
unknown <mostofa.reza3@gmail.com>
2023-06-03 16:41:46 +0600

    [Done]-Login functionality

---
+++ app/actions/getCurrentUser.ts
@@ -0,0 +1,26 @@
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth/next";
+
+import prisma from "../../app/lib/prismaDB";
+
+export async function getSession() {
+  return await getServerSession(authOptions);
+}
+
+export default async function getCurrentUser() {
+  try {
+    const session = await getSession();
+    if (!session?.user?.email) {
+      return null;
+    }
+    const user = await prisma.user.findUnique({
+      where: { email: session.user.email as string },
+    });
+
+    if (!user) return null;
+
+    return user;
+  } catch (error: any) {
+    return null;
+  }
+}
